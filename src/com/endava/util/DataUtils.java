68c4946a7c63ec1296cacf534bb461e305fcdbed
Stason1o <stasikus1996@gmail.com>
2017-03-25 23:13:41 +0200

    delete overriden methods

--- src/com/endava/util/DataUtils.java
+++ src/com/endava/util/DataUtils.java
@@ -10,16 +10,6 @@ public class DataUtils {
         return array;
     }
 
-    public double[] createArray(double a, double b){
-        double array[] = new double[(int)(a * b)];
-        return array;
-    }
-
-    public float[] createArray(float a, float b){
-        float array[] = new float[(int)( a * b)];
-        return array;
-    }
-
     public int[] fillArray(int[] array){
         for (int i = 0; i < array.length; i++){
             array[i] = (int)(Math.random() * 500);
@@ -27,41 +17,14 @@ public class DataUtils {
         return array;
     }
 
-    public double[] fillArray(double[] array){
-        for (int i = 0; i < array.length; i++){
-            array[i] = Math.random() * 500;
-        }
-        return array;
-    }
-
-    public float[] fillArray(float[] array){
-        for (int i = 0; i < array.length; i++){
-            array[i] = (float)(Math.random() * 500);
-        }
-        return array;
-    }
-
-
     public void printArray(int[] array){
         for (int i = 0; i < array.length; i++)
             System.out.print(array[i] + " ");
         System.out.println();
     }
 
-    public void printArray(double[] array){
-        for (int i = 0; i < array.length; i++)
-            System.out.print(array[i] + " ");
-        System.out.println();
-    }
-
-    public void printArray(float[] array){
-        for (int i = 0; i < array.length; i++)
-            System.out.print(array[i] + " ");
-        System.out.println();
-    }
-
-    public double[] sortArray(double[] array){
-        double tmp;
+    public int[] sortArray(int[] array){
+        int tmp;
         for(int i = 0; i < array.length - 1; i++){
             for(int j = i; j < array.length; j++){
                 if(array[i] > array[j]) {
